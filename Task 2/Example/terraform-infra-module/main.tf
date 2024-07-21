0af0887935c1a8ff8cb5e7643c2acb72d0b56b17
dhruvipatel112 <97799450+dhruvipatel112@users.noreply.github.com>
2024-07-21 17:39:44 +0530

    Update main.tf

--- Task 2/Example/terraform-infra-module/main.tf
+++ Task 2/Example/terraform-infra-module/main.tf
@@ -35,3 +35,20 @@ resource "google_cloud_run_service" "cloud_run_service" {
     latest_revision = true
   }
 }
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.cloudrun.location
+  project     = google_cloud_run_service.cloudrun.project
+  service     = google_cloud_run_service.cloudrun.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
